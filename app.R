a9859d642529175b02573339e223d0448a0ebca2
Hugo Lepage <hugo_lep@hotmail.com>
2025-06-14 14:18:13 -0400

    premier commit

---
+++ app.R
@@ -0,0 +1,24 @@
+library(shiny)
+
+# Définition de l'interface utilisateur (UI)
+ui <- fluidPage(
+  titlePanel("Mini App Shiny"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("num", "Choisissez un nombre :", 1, 100, 50)
+    ),
+    mainPanel(
+      textOutput("result")
+    )
+  )
+)
+
+# Définition du serveur (Server)
+server <- function(input, output) {
+  output$result <- renderText({
+    paste("Vous avez choisi le nombre :", input$num)
+  })
+}
+
+# Exécution de l'application Shiny
+runApp(list(ui = ui, server = server))
