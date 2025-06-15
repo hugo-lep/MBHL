fbd4bf8db0a6df4d61f8668e663fd16311f23349
Hugo Lepage <hugo_lep@hotmail.com>
2025-06-15 11:00:43 -0400

    remis les fichiers mtce shinydashboard dans l'app

--- R/protegR.R
+++ R/protegR.R
@@ -1,36 +1,67 @@
-library(shinydashboard)
+library(shinydashboard) # sur EC2: lu avant shinydashboard
+
+# UI ----------------------------------------------------------------------
 
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Mini Dashboard"),
+  dashboardHeader(title = "Mini App"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Accueil", tabName = "accueil", icon = icon("home")),
-      menuItem("Stats", tabName = "stats", icon = icon("chart-bar"))
+      menuItem("Accueil", tabName = "home", icon = icon("home")),
+      menuItem("Graphique", tabName = "plot", icon = icon("chart-line")),
+      menuItem("Maintenance", icon = icon("screwdriver-wrench"),
+               menuSubItem("AC inspection",tabName = "AC_inspection", icon = icon("list")),
+               menuSubItem("Store",tabName = "warehouse", icon = icon("warehouse")))
     )
   ),
   dashboardBody(
+    tags$head(
+      # Icône par défaut (flèche vers la gauche)
+      tags$style(HTML("
+        .main-header .sidebar-toggle:before {
+          content: none; /* Ne rien afficher par défaut */
+        }
+      ")),
+      # Script pour changer l’icône selon l’état de la sidebar
+      tags$script(HTML("
+        $(document).on('shiny:connected', function() {
+          function updateIcon() {
+            if ($('body').hasClass('sidebar-collapse')) {
+              $('.sidebar-toggle').html('<i class=\"fa fa-angle-right\"></i>');
+            } else {
+              $('.sidebar-toggle').html('<i class=\"fa fa-angle-left\"></i>');
+            }
+          }
+
+          $('.sidebar-toggle').on('click', function() {
+            setTimeout(updateIcon, 300); // attendre l'animation
+          });
+
+          updateIcon(); // initialisation
+        });
+      "))
+    ),
     tabItems(
-      tabItem(tabName = "accueil",
-              h2("Bienvenue sur la page d'accueil"),
-              actionButton("btn", "Clique ici")
+      tabItem(tabName = "home",
+              h2("Bienvenue dans l'application !")
       ),
-      tabItem(tabName = "stats",
-              h2("Statistiques"),
-              verbatimTextOutput("compteur")
-      )
+      tabItem(tabName = "plot",
+              sliderInput("n", "Nombre d'observations", 10, 100, 50),
+              plotOutput("hist")
+      ),
+      tabItem(tabName = "AC_inspection",
+              h2("page pour le suivi des avions")),
+      tabItem(tabName = "warehouse",
+              h2("page pour les pièces"))
     )
   )
 )
 
+
+# SERVER ------------------------------------------------------------------
+
 server <- function(input, output, session) {
-  compteur <- reactiveVal(0)
-
-  observeEvent(input$btn, {
-    compteur(compteur() + 1)
-  })
-
-  output$compteur <- renderText({
-    paste("Bouton cliqué", compteur(), "fois")
+  output$hist <- renderPlot({
+    hist(rnorm(input$n), main = "Histogramme", col = "steelblue")
   })
 }
--- app.R
+++ app.R
@@ -1,7 +1,28 @@
+# Data for all sessions  ---------------------------------------------------------
+# * ------ library --------------------------------------------------------
+
+library(here)
 library(shiny)
+library(protegR)
+library(tidyr)
+library(purrr)
+
+
+# * ------ AWS connect + load config --------------------------------------
+#AWS_connection()
+#config_global <- s3readRDS(object = str_c(config_s3_location$s3_main_folder,"/config_files/config_global.rds"),
+#                           bucket = config_s3_location$s3_bucket)
+#addResourcePath("images", "inst/app/www")
+
+
+# * ------ load R files ------------------------------------------------------------
+list.files("R", full.names = TRUE, pattern = "\\.R$") %>% walk(source)
+#list.files("R/internal", full.names = TRUE, pattern = "\\.R$") %>% walk(source)
 
 
 
+
+
+
+# START APP ---------------------------------------------------------------
 shinyApp(ui, server)
-
-
